45746e5a917582e48546103d58a4a241947cb882
rezafitriaman <rezafitriaman@gmail.com>
2017-11-01 14:45:15 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+console.log('Hello Reza');
+/*active slick slider on frontpage*/
+    $('.slider-item').slick({
+    });
+
+});
+
+ /*================================= oudside document ready (initMap) ===============================*/
